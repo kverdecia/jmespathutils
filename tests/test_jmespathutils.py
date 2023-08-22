1d395c4f87f59e1fe7c51b99603da9030db156ed
Karel Antonio Verdecia Ortiz <kverdecia@gmail.com>
2023-08-22 00:12:24 -0300

    initial commit

---
+++ tests/test_jmespathutils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `jmespathutils` package."""
+
+
+import unittest
+
+from jmespathutils import jmespathutils
+
+
+class TestJmespathutils(unittest.TestCase):
+    """Tests for `jmespathutils` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
