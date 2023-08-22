255cca4b616c8aac6c8c60bdc7f80483900ab741
Karel Antonio Verdecia Ortiz <kverdecia@gmail.com>
2023-08-22 01:28:50 -0300

    initial functionality

--- setup.py
+++ setup.py
@@ -4,41 +4,49 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
-
-test_requirements = [ ]
+test_requirements: list[str] = []
 
 setup(
+    extras_require={
+        "dev": [
+            "mypy",
+            "black",
+            "vistir==0.6.1",
+            "pipenv-setup[black]",
+            "pre-commit",
+            "types-jmespath",
+            "tox",
+        ]
+    },
     author="Karel Antonio Verdecia Ortiz",
-    author_email='kverdecia@gmail.com',
-    python_requires='>=3.6',
+    author_email="kverdecia@gmail.com",
+    python_requires=">=3.10",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     description="Functions for jmespath",
-    install_requires=requirements,
+    install_requires=["jmespath", "xmltodict", "cuid"],
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='jmespathutils',
-    name='jmespathutils',
-    packages=find_packages(include=['jmespathutils', 'jmespathutils.*']),
-    test_suite='tests',
+    keywords="jmespathutils",
+    name="jmespathutils",
+    packages=find_packages(include=["jmespathutils", "jmespathutils.*"]),
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/kverdecia/jmespathutils',
-    version='0.1.0',
+    url="https://github.com/kverdecia/jmespathutils",
+    version="0.1.0",
     zip_safe=False,
 )
